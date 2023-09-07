dd81eec481b3eec160e68497d50e972ab299f441
sibzal <sibzal5@gmail.com>
2023-09-07 12:41:12 +0300

    dfdfdf

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+
+let name_sity = prompt('Введите название города')
+let year_sity = prompt('Введите год образования')
+let count_people = prompt('Население города')
+console.log('Городу ' + name_sity + ' исполнилось ' + (2023-Number(year_sity)) 
++ ' лет с момента образования. Население: ' + Number(count_people))
