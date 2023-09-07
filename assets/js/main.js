21f0c2cd4aa3c55462608cadd4d2647afa837e1f
sibzal <142913222+sibzal@users.noreply.github.com>
2023-09-07 13:03:46 +0300

    d main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,5 @@
 let name_sity = prompt('Введите название города')
 let year_sity = prompt('Введите год образования')
 let count_people = prompt('Население города')
-console.log('Городу ' + name_sity + ' исполнилось ' + (2023-Number(year_sity)) 
+alert('Городу ' + name_sity + ' исполнилось ' + (2023-Number(year_sity)) 
 + ' лет с момента образования. Население: ' + Number(count_people))
